5cdd264f55d341f64f12c434a64a9325dedbb76b
Juan Martin Fajardo <jmarfone1@gmail.com>
2026-05-08 20:53:21 +0200

    runtime.txt added

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, session, jsonify
 import base_datos
 from flask_socketio import SocketIO, emit, join_room, leave_room
